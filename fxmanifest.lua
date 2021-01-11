8ab8cac907b4ebe3e2867f8b2a40ab1fe2cf20e4
Pedro Galembeck <51977156+LDNzera@users.noreply.github.com>
2021-01-11 19:21:22 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'LDNzera'
+contact 'E-mail: ldnzeragaming@gmail.com - Discord: Hailee#8793'
+version '1.0.3'
+
+ui_page 'nui/moneywash.html'
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'chihuahua/*.lua'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'dachshund/*.lua'
+}
+
+files {
+  'nui/*',
+  'nui/**/*'
+}
